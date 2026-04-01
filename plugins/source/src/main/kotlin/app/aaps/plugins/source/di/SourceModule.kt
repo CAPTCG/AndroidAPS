27f702902dafa5c8c3a87788175dc670e2cc559e
Craig Gordon <craighvac@gmail.com>
2026-04-01 19:13:27 -0400

    Add Eversense E3/365 CGM plugin
    - Add plugins:eversense module with full BLE driver for E3 and 365 transmitters
    - Add EversensePlugin integrating with AAPS BgSource interface
    - Add placement guide activity with 500ms signal polling
    - Add calibration activity
    - Add Bluetooth permission request activity
    - Add EVERSENSE_E3 and EVERSENSE_365 to SourceSensor and GlucoseValue enums
    - Add Eversense notification IDs to NotificationId enum
    - Register plugin in SourceModule, PluginsListModule and settings.gradle
    - Use new AAPS NotificationManager API for all notifications
    - Support both Eversense E3 (180-day) and Eversense 365 (1-year) transmitters

--- plugins/source/src/main/kotlin/app/aaps/plugins/source/di/SourceModule.kt
+++ plugins/source/src/main/kotlin/app/aaps/plugins/source/di/SourceModule.kt
@@ -4,6 +4,7 @@ import app.aaps.core.interfaces.source.DexcomBoyda
 import app.aaps.core.interfaces.source.NSClientSource
 import app.aaps.core.interfaces.source.XDripSource
 import app.aaps.plugins.source.DexcomPlugin
+import app.aaps.plugins.source.EversensePlugin
 import app.aaps.plugins.source.GlimpPlugin
 import app.aaps.plugins.source.MM640gPlugin
 import app.aaps.plugins.source.NSClientSourcePlugin
@@ -13,7 +14,10 @@ import app.aaps.plugins.source.PoctechPlugin
 import app.aaps.plugins.source.SyaiPlugin
 import app.aaps.plugins.source.TomatoPlugin
 import app.aaps.plugins.source.XdripSourcePlugin
+import app.aaps.plugins.source.activities.EversenseCalibrationActivity
+import app.aaps.plugins.source.activities.EversensePlacementActivity
 import app.aaps.plugins.source.activities.RequestDexcomPermissionActivity
+import app.aaps.plugins.source.activities.RequestEversensePermissionActivity
 import app.aaps.plugins.source.instara.InstaraPlugin
 import app.aaps.plugins.source.instara.InstaraStaleCheckWorker
 import app.aaps.plugins.source.notificationreader.NotificationCollectorService
@@ -31,7 +35,6 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 @Suppress("unused")
 abstract class SourceModule {
-
     @ContributesAndroidInjector abstract fun contributesXdripWorker(): XdripSourcePlugin.XdripSourceWorker
     @ContributesAndroidInjector abstract fun contributesDexcomWorker(): DexcomPlugin.DexcomWorker
     @ContributesAndroidInjector abstract fun contributesMM640gWorker(): MM640gPlugin.MM640gWorker
@@ -41,18 +44,18 @@ abstract class SourceModule {
     @ContributesAndroidInjector abstract fun contributesSyaiWorker(): SyaiPlugin.SyaiWorker
     @ContributesAndroidInjector abstract fun contributesSiAppWorker(): PatchedSiAppPlugin.PatchedSiAppWorker
     @ContributesAndroidInjector abstract fun contributesSinoAppWorker(): PatchedSinoAppPlugin.PatchedSinoAppWorker
-
     @ContributesAndroidInjector abstract fun contributesRequestDexcomPermissionActivity(): RequestDexcomPermissionActivity
+    @ContributesAndroidInjector abstract fun contributesEversensePlugin(): EversensePlugin
+    @ContributesAndroidInjector abstract fun contributesRequestEversensePermissionActivity(): RequestEversensePermissionActivity
+    @ContributesAndroidInjector abstract fun contributesEversenseCalibrationActivity(): EversenseCalibrationActivity
+    @ContributesAndroidInjector abstract fun contributesEversensePlacementActivity(): EversensePlacementActivity
     @ContributesAndroidInjector abstract fun contributesNotificationCollectorService(): NotificationCollectorService
-
-    // Instara related worker
     @ContributesAndroidInjector abstract fun contributesInstaraWorker(): InstaraPlugin.InstaraWorker
     @ContributesAndroidInjector abstract fun contributesInstaraStaleCheckWorker(): InstaraStaleCheckWorker
 
     @Module
     @InstallIn(SingletonComponent::class)
     interface Bindings {
-
         @Binds fun bindNSClientSource(nsClientSourcePlugin: NSClientSourcePlugin): NSClientSource
         @Binds fun bindDexcomBoyda(dexcomPlugin: DexcomPlugin): DexcomBoyda
         @Binds fun bindXDrip(xdripSourcePlugin: XdripSourcePlugin): XDripSource
