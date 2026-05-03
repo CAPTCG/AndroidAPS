1a89459f520a95c25b2b38d3c4e22457faaf8c1e
Craig Gordon <craighvac@gmail.com>
2026-05-03 15:49:21 -0400

    fix: remove enableOmnipodDriftCompensation override that no longer exists in Config

--- app/src/main/kotlin/app/aaps/implementations/ConfigImpl.kt
+++ app/src/main/kotlin/app/aaps/implementations/ConfigImpl.kt
@@ -74,4 +74,6 @@ class ConfigImpl @Inject constructor(
         enabledOptionsCache.getOrPut(option) {
             fileListProvider.get().ensureExtraDirExists()?.findFile(option.filename) != null
         }
+    override fun enableOmnipodDriftCompensation(): Boolean = isEnabled(ExternalOptions.OMNIPOD_DRIFT_COMPENSATION)
 }
+
